936565f8f9c5c979b453009dfa0d9bbb8298e45e
reese parsons <reeseparsons99@gmail.com>
2024-01-22 12:12:36 -0400

    simplified the code, removed the sdf variable

--- src/App.java
+++ src/App.java
@@ -6,28 +6,20 @@ import java.util.Date;
 public class App {
     public static void main(String[] args) throws Exception {
         Date currentDate = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat();
 
-        sdf.applyPattern("yyyy");
-        JOptionPane.showMessageDialog(new JDialog(), sdf.format(currentDate), "Year", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(new JDialog(), new SimpleDateFormat("yyyy").format(currentDate), "Year", JOptionPane.PLAIN_MESSAGE);
 
-        sdf.applyPattern("MMMM");
-        JOptionPane.showMessageDialog(new JDialog(), sdf.format(currentDate), "Month", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(new JDialog(), new SimpleDateFormat("MMMM").format(currentDate), "Month", JOptionPane.PLAIN_MESSAGE);
 
-        sdf.applyPattern("dd");
-        JOptionPane.showMessageDialog(new JDialog(), sdf.format(currentDate), "Day", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(new JDialog(), new SimpleDateFormat("dd").format(currentDate), "Day", JOptionPane.PLAIN_MESSAGE);
 
-        sdf.applyPattern("HH");
-        JOptionPane.showMessageDialog(new JDialog(), sdf.format(currentDate), "Hour", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(new JDialog(), new SimpleDateFormat("HH").format(currentDate), "Hour", JOptionPane.PLAIN_MESSAGE);
 
-        sdf.applyPattern("mm");
-        JOptionPane.showMessageDialog(new JDialog(), sdf.format(currentDate), "Minutes", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(new JDialog(), new SimpleDateFormat("mm").format(currentDate), "Minutes", JOptionPane.PLAIN_MESSAGE);
 
-        sdf.applyPattern("ss");
-        JOptionPane.showMessageDialog(new JDialog(), sdf.format(currentDate), "Seconds", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(new JDialog(), new SimpleDateFormat("ss").format(currentDate), "Seconds", JOptionPane.PLAIN_MESSAGE);
 
-        sdf.applyPattern("SSS");
-        JOptionPane.showMessageDialog(new JDialog(), sdf.format(currentDate), "Milliseconds", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(new JDialog(), new SimpleDateFormat("SSS").format(currentDate), "Milliseconds", JOptionPane.PLAIN_MESSAGE);
 
         System.exit(0);
     }
